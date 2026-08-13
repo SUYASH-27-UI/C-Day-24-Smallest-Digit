0d3581e7311178cec0a35bb8ef0ded0f6f158a16
SUYASH-27-UI <suyashsurve30@gmail.com>
2026-08-13 10:53:07 +0530

    Create smallest_digit.c

---
+++ smallest_digit.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    int digit;
+    int smallest = 9;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num != 0)
+    {
+        digit = num % 10;
+
+        if (digit < smallest)
+        {
+            smallest = digit;
+        }
+
+        num = num / 10;
+    }
+
+    printf("Smallest digit = %d", smallest);
+
+    return 0;
+}
